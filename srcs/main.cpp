8ba68c1ce7e5c6785b63ce59c07fda4993c3ff81
Julien CALON <jcalon@e2r5p7.clusters.42paris.fr>
2022-12-08 13:33:33 +0100

    Commit

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -11,7 +11,7 @@ int main(int ac, char **av)
 	try
 	{
 		Config Config(av[ac - 1]);
-		std::cout << Config << std::endl;
+		// std::cout << Config << std::endl;
 		if (Config.get_error() == true)
 		{
 			std::cout << RED << "ERROR: " << RESET << "Server can't be configurated, go check your .conf file..." << std::endl;
