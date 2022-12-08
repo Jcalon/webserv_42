f972ebe318ccf02b00dbd9e831bd422c8fde828a
Julien CALON <jcalon@e2r5p7.clusters.42paris.fr>
2022-12-08 13:05:19 +0100

    Commit

--- www/site/time.php
+++ www/site/time.php
@@ -1,18 +1,27 @@
 <!DOCTYPE html>
 
 <head>
+    <script src="https://kit.fontawesome.com/191e243f28.js"></script>
     <title>What time</title>
 </head>
 
 <body style="text-align: center;">
+<h1>Date</h1>
+<i class="fa-solid fa-calendar-days"></i>
 <?php
 // Set the new timezone
-date_default_timezone_set('Europe/Paris');
-$date = date('d-m-y h:i:s');
-echo $date;
+    date_default_timezone_set('Europe/Paris');
+    $date = date('d-m-y');
+    echo $date;
+?>
+<h1>Time</h1>
+<i class="fa-regular fa-clock"></i>
+<?php
+// Set the new timezone
+    date_default_timezone_set('Europe/Paris');
+    $date = date('h:i:s');
+    echo $date;
 ?>
 
 </body>
 </html>
-
-
